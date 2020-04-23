39e9cf6239b43c1160e723c75aba05a5dc2a676b
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-04-23 21:34:48 +0200

    Import createStore from redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import todoApp from "./reducers";
 import "./index.css";
 import App from "./App";
+
+const store = createStore(todoApp);
 
 ReactDOM.render(
   <React.StrictMode>
