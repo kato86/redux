7cc099e5ba49e82c9c3b936344123e9e5e6c2370
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-04-26 22:51:10 +0200

    Import VisibilityFilter and add to switch - case

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,32 +1,27 @@
 import { connect } from "react-redux";
 import { toggleTodo } from "../actions";
 import TodoList from "../components/TodoList";
+import { VisibilityFilter } from "../actions";
 
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
-    case "SHOW_ALL":
+    case VisibilityFilter.SHOW_ALL:
       return todos;
-    case "SHOW_COMPLETED":
+    case VisibilityFilter.SHOW_COMPLETED:
       return todos.filter((t) => t.completed);
-    case "SHOW_ACTIVE":
+    case VisibilityFilter.SHOW_ACTIVE:
       return todos.filter((t) => !todos.completed);
+    default:
+      throw new Error("Unknown filter: " + filter);
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    todos: getVisibleTodos(state.todos, state.visibilityFilter),
-  };
-};
+const mapStateToProps = (state) => ({
+  todos: getVisibleTodos(state.todos, state.visibilityFilter),
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onTodoClick: (id) => {
-      dispatch(toggleTodo(id));
-    },
-  };
-};
+const mapDispatchToProps = (dispatch) => ({
+  toggleTodo: (id) => dispatch(toggleTodo(id)),
+});
 
-const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList);
-
-export default VisibleTodoList;
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
