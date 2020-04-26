06a4c0a22427f82541f988e96fdfa09f88e297ba
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-04-26 22:22:15 +0200

    Add folder action and index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import todoApp from "./reducers";
+import rootReducer from "./reducers";
 import App from "./components/App";
 
-const store = createStore(todoApp);
+const store = createStore(rootReducer);
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
