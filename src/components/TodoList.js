809a9d4c25529392073eecdd9d44fa265928105f
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-04-26 22:40:42 +0200

    Add button to Link.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import Todo from "./Todo";
 
-const TodoList = ({ todos, onTodoClick }) => (
+const TodoList = ({ todos, toggleTodo }) => (
   <ul>
-    {todos.map((todo, index) => (
-      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+    {todos.map((todo) => (
+      <Todo key={todo.id} {...todo} onClick={() => toggleTodo(todo.id)} />
     ))}
   </ul>
 );
@@ -18,7 +18,7 @@ TodoList.propTypes = {
       text: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
-  onClick: PropTypes.func.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
 };
 
 export default TodoList;
